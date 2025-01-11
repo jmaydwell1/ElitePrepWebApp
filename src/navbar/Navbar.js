601185e6f368d88d73df4d383f0d3ca3bde38f78
jmaydwell1 <maydwell.j@northeastern.edu>
2025-01-10 19:26:03 -0500

    corrected logo added

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,9 +1,9 @@
-import image from '../Frame 1.svg';
+import image from '../brandmark-design 1.svg';
 
 const Navbar = () => {
     return(
         <div className="flex items-center justify-between h-28">
-            <img src={image} alt="Elite Prep Logo" className="mx-4 w-60 h-60 hover:opacity-75"/>
+            <img src={image} alt="Elite Prep Logo" className="mx-6 w-60 h-60 hover:opacity-75"/>
             <button className="bg-neutral-800 hover:opacity-85 text-gray-400 py-5 px-6 mx-12 my-8 rounded-full">Download</button>
         </div>
     )
